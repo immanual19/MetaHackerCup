f34afebc01051a3399e630c2c10c94d67aad54a0
A. Immanual Sarker <66303689+immanual19@users.noreply.github.com>
2022-09-29 00:06:45 +0600

    Add files via upload

---
+++ 2011A.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+long long int solve(long long int n){
+    unordered_set<int>us;
+    for(int i=0;i<=sqrt(n);i++){
+        
+        long long int square=i*i;
+        long long int diff=n-square;
+        long long int sqrtDiff=sqrt(diff);
+        if(sqrtDiff*sqrtDiff==diff){
+                us.insert(i+sqrtDiff);
+        }
+    }
+    return us.size();
+    
+}
+int main() {
+    
+    int t,i=1;
+    cin>>t;
+    while(t--){
+        long long int n;
+        cin>>n;
+        cout<<"Case #"<<i<<": "<<solve(n)<<endl;
+        i++;
+    }
+    
+    return 0;
+}
